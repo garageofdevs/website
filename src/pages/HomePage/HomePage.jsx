5a3dbf53fb3fd4911f58f5d8c77024577f1a4cef
ShakthiNR <shakthinr.garageofdevs@gmail.com>
2024-11-18 22:09:57 +0530

    responsive ness improved

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,30 @@
-import  GarageofdevsMainLogo from "../../assets/images/garageofdevsMainLogo.svg?react";
-import styles from './styles/HomePage.module.css'; 
-import SocialsComponent from './components/SocialsComponent';
+import GarageofdevsMainLogo from "../../assets/images/garageofdevsMainLogo.svg?react";
+import styles from "./styles/HomePage.module.css";
+import SocialsComponent from "./components/SocialsComponent";
 function HomePage() {
   return (
     <div className={styles.mainContent}>
-        <div className={styles.coreContent}>
+      <div className={styles.coreContent}>
         <div className={styles.logoAndBrand}>
+   
             <GarageofdevsMainLogo className={styles.logo} />
-            <p  className={styles.brandName}>Garage Of Devs</p>
+        
+          <div style={{ position: "absolute", bottom: "-0.5em" }}>
+            <p className={styles.brandName}>Garage Of Devs</p>
+          </div>
         </div>
-        <div className={styles.description}></div>
+        
         <p className={styles.brandDesc}>
-        Garage Of Devs is an dynamic software development firm specializing in crafting solutions, we excel in the building platforms and custom websites. 
-        Our mission is to construct innovative platforms and empower clients across diverse industries by enhancing their digital presence.
+          Garage Of Devs is an dynamic software development firm specializing in
+          crafting solutions, we excel in the building platforms and custom
+          websites. Our mission is to construct innovative platforms and empower
+          clients across diverse industries by enhancing their digital presence.
         </p>
         <SocialsComponent />
-        </div>
-        <div className={styles.copyright}>© 2024 Garage of Devs </div>
+      </div>
+      <div className={styles.copyright}>© 2024 Garage of Devs </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/pages/HomePage/components/SocialsComponent.jsx
+++ src/pages/HomePage/components/SocialsComponent.jsx
@@ -1,19 +1,18 @@
-import styles from '../styles/HomePage.module.css'; 
-import  Instagram from "../../../assets/images/instagram.svg?react";
-import  LinkedIn from "../../../assets/images/linkedIn.svg?react";
-import  Gmail from "../../../assets/images/gmail.svg?react";
+import styles from "../styles/HomePage.module.css";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 
 function SocialsComponent() {
   return (
     <>
-        <p  className={styles.socialTitle}>Social Links</p>
-        <div className={styles.socialLinksDiv}>
-            <a href='https://www.linkedin.com/company/garage-of-devs/'><LinkedIn className={styles.linkedIn} /></a>
-            <a href='mailto:garageofdevs@gmail.com'><Gmail className={styles.gmail} /></a>
-            <a href='https://www.instagram.com/garageofdevs/'><Instagram className={styles.instagram} /></a>
-        </div>
+      <p className={styles.socialTitle}>Social Links</p>
+      <div className={styles.socialLinksDiv}>
+        <FaLinkedin className={styles.icon} />
+        <BiLogoGmail className={styles.icon} />
+        <FaInstagram className={styles.icon} />
+      </div>
     </>
-  )
+  );
 }
 
-export default SocialsComponent
+export default SocialsComponent;
