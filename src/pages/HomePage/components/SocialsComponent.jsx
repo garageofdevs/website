45af5292e8b8a00aeca341e1edef12a027dbe990
ShakthiNR <shakthinr.garageofdevs@gmail.com>
2024-11-18 23:03:08 +0530

    completed

--- src/pages/HomePage/components/SocialsComponent.jsx
+++ src/pages/HomePage/components/SocialsComponent.jsx
@@ -3,13 +3,37 @@ import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 
 function SocialsComponent() {
+  function handleClick(url) {
+    window.open(url, "_blank");
+  }
   return (
     <>
       <p className={styles.socialTitle}>Social Links</p>
       <div className={styles.socialLinksDiv}>
-        <FaLinkedin className={styles.icon} />
-        <BiLogoGmail className={styles.icon} />
-        <FaInstagram className={styles.icon} />
+        <FaLinkedin
+          onClick={() =>
+            handleClick("https://www.linkedin.com/company/garage-of-devs")
+          }
+          size={20}
+          className={styles.icon}
+        />
+        <a
+          href="mailto:garageofdevs@gmail.com"
+          target="_top"
+          style={{
+            color: "black",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <BiLogoGmail className={styles.icon} size={21} />
+        </a>
+        <FaInstagram
+          onClick={() => handleClick("https://www.instagram.com/garageofdevs/")}
+          size={20}
+          className={styles.icon}
+        />
       </div>
     </>
   );
